0fb56b4bb74bb23aa725d9a9a46b34dc20088fb9
asa9874 <pjkpjk1129@gmail.com>
2025-05-17 18:17:21 +0900

    Feat: 로그인 페이지 제작, 기본 폰트 설정

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,25 @@
 function Login() {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="flex items-center w-full h-full  flex-col p-3 justify-center bg-sky-300 rounded-2xl">
+        <form className="flex flex-col items-center justify-center w-full h-full ">
+          <h1 className="text-5xl font-bold mb-10">로그인</h1>
+          <input
+            type="text"
+            placeholder="아이디"
+            className="w-[80%] h-[40px] p-2 m-2 border border-gray-300 rounded"
+          />
+          <input
+            type="password"
+            placeholder="비밀번호"
+            className="w-[80%] h-[40px] p-2 m-2 border border-gray-300 rounded"
+          />
+          <button className="w-[80%] h-[40px] p-2 m-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+            로그인
+          </button>
+          <button className="w-[80%] h-[40px] p-2 m-2 bg-green-500 text-white rounded hover:bg-green-600">
+            회원가입
+          </button>
+        </form>
     </div>
   );
 }
