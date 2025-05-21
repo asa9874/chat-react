7c420f0e33a44c5e076f40e2ec89bff8c759aa89
asa9874 <pjkpjk1129@gmail.com>
2025-05-21 14:34:53 +0900

    Feat: loginMember 함수에 반환 타입 추가 및 에러 처리 개선

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,10 +1,12 @@
+import type { AuthToken } from "../types/AuthToken";
 import apiClient from "./apiClient";
 
-export async function loginMember(memberData: { email: string; password: string }) {
+export async function loginMember(memberData: { email: string; password: string }): Promise<AuthToken> {
     try {
         const response = await apiClient.post(`/auth/login`, memberData);
         return response.data;
     } catch (error) {
         console.error(error);
+        throw error;
     }
 }
